527d71fe3e1a32a31cf1161993d9f0d3b5e6aef4
Dmitriy <Dimamakarov1941@yandex.ru>
2021-06-12 11:28:10 +0300

    add build

--- Assets/Scripts/GameManages/OnPuzzleClick3.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-public class OnPuzzleClick3 : MonoBehaviour {
-    [SerializeField] private PuzzleManagerLarge3 PuzzleManager = null;
-    [SerializeField] private int ID = 0;
-    private void OnMouseDown () => PuzzleManager.onAdPuzlleClick (this.gameObject, ID);
-}
--- Assets/Scripts/GameManages/OnPuzzleClick5.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public class OnPuzzleClick5 : MonoBehaviour {
-  [SerializeField] private PuzzleManager5 PuzzleManager = null;
-  [SerializeField] private int ID = 0;
-
-  private void OnMouseDown () => PuzzleManager.onAdPuzlleClick (this.gameObject, ID);
-}
